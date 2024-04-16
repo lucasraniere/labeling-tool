fb62e0f2cb2d0a8b5858cf3fcb1cea6054a610f7
Lucas Raniére <lucas.raniere94@gmail.com>
2024-04-16 13:57:03 -0400

    updating repo

--- labeling-tool/src/pages/components/basics/TweetNavigation.tsx
+++ labeling-tool/src/pages/components/basics/TweetNavigation.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react"
+
 export default function TweetNavigation() {
+    const [tweetId, setTweetId] = useState<number>();
     return (
         <div>
             <div>
                 <span>Id: </span>
-                <textarea id='id-to-go' name='to-go-id' rows='1' cols='6'></textarea>
+                <textarea value={tweetId} id='id-to-go' name='to-go-id' rows='1' cols='6'/>
                 <button>Ir</button>
                 <button>Ir para próximo</button>
             </div>
